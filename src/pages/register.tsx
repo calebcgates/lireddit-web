ee919c4e9ec87828eebbc181e4cf75566fbd148d
Caleb Gates <caleb.gates@henderex.com>
2020-11-22 20:19:15 -0500

    username formik field created

---
+++ src/pages/register.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import {Form, Formik} from 'formik'
+import {FormControl, FormLabel, Input} from '@chakra-ui/react';
+
+interface registerProps {
+
+}
+
+
+
+export const Register: React.FC<registerProps> = ({}) => {
+    return (
+        <Formik
+            initialValues={{username:"", password:""}}
+            onSubmit={(values) => {
+                console.log(values);
+            }}
+        >
+            {({values, handleChange})=>(
+                <Form>
+                    <FormControl>
+                        <FormLabel htmlFor="username">Username</FormLabel>
+                        <Input value={values.username} onChange={handleChange} id="name" placeholder="name" />
+                        {/*<FormErrorMessage>{form.errors.name}</FormErrorMessage>*/}
+                    </FormControl>
+                </Form>
+            )}
+        </Formik>
+    );
+};
+
+export default Register
